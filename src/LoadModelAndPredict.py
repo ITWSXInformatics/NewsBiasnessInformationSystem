0eb617ac8002ad7193f3e1dea9f49adc8b07a63d
brendanCross86 <brendan.cross86@gmail.com>
2020-05-03 01:53:15 -0400

    bleh

--- src/LoadModelAndPredict.py
+++ src/LoadModelAndPredict.py
@@ -3,12 +3,91 @@ import argparse
 import spacy
 import numpy as np
 import pickle
+import os
 
 STOPWORDS = {
     'say', 'not', 'like', 'go', "be", "have", "s", #original
     "and", "when", "where", "who", "let", "look", "time", "use", "him", "her",
     "she", "he"
 }
+
+
+def loadModules(models_dir="models"):
+    """
+    Load our LDA and
+
+    :return:
+    """
+    lda_path = os.path.join(models_dir, "LDA_Model.model")
+    classifier_path = os.path.join(models_dir, "Classifer.pkl")
+
+
+    print("Loading LDA model")
+    lda = LdaModel.load(lda_path)
+    id2word = lda.id2word
+    print("Finished loading LDA model")
+
+    print("Loading Classifier")
+    try:
+        with open(classifier_path, 'rb') as f:
+            classifier = pickle.load(f)
+            print("Finished loading classifier!")
+    except:
+        classifer = None
+        print("Failed to load classifier!")
+
+    print("Loading spacy")
+    nlp = spacy.load('en_core_web_md')
+    print("Finished loading spacy")
+
+    return lda, classifer, nlp
+
+
+def predict(article_data):
+    """
+    Predict the class and topics of the given article data.
+
+    :param article_data:
+    :return:
+    """
+
+    lda, classifier, nlp = loadModules()
+    id2word = lda.id2word
+
+    documents = []
+    raw_texts = []
+    with nlp.disable_pipes("ner"):
+        text_raw = article_data['text']
+        raw_texts.append(text_raw)
+
+        text = text_raw.replace("\n", " ")
+
+        # preprocessing text
+        lem_text = [token.lemma_.lower() for token in nlp(text)
+                    if not token.is_stop
+                    and not token.is_punct
+                    and not token.is_space
+                    and not token.lemma_.lower() in STOPWORDS
+                    and not token.pos_ == 'SYM'
+                    and not token.pos_ == 'NUM']
+        # print(lem_text)
+        documents.append(lem_text)
+
+    corpus = [id2word.doc2bow(doc) for doc in documents]
+    topic_vecs = []
+    for doc_as_corpus in corpus:
+        top_topics = lda.get_document_topics(doc_as_corpus,
+                                             minimum_probability=0)
+        topic_vec = [top_topics[i][1] for i in range(lda.num_topics)]
+        topic_vecs.append(np.array(topic_vec).reshape(1, -1))
+
+    for i in range(len(topic_vecs)):
+        print(raw_texts[i])
+        print(topic_vecs[i])
+        print("prediction: {}".format(classifier.predict(topic_vecs[i])))
+
+    return
+
 
 def main():
 
